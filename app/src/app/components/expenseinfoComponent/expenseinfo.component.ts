c8fddcef0f5d1acdea24420b5cb3e7c5d6b887a1
Roja1997 <rojasanmarga@gmail.com>
2019-02-12 18:54:36 +0530

    storing image in mobile gallery

--- app/src/app/components/expenseinfoComponent/expenseinfo.component.ts
+++ app/src/app/components/expenseinfoComponent/expenseinfo.component.ts
@@ -9,6 +9,9 @@ import { NBaseComponent } from '../../../../../app/baseClasses/nBase.component';
  * Service import Example :
  * import { HeroService } from '../services/hero/hero.service';
  */
+declare const navigator: any;
+declare const cordova: any;
+declare const textocr: any;
 
 @Component({
     selector: 'bh-expenseinfo',
@@ -17,13 +20,16 @@ import { NBaseComponent } from '../../../../../app/baseClasses/nBase.component';
 
 export class expenseinfoComponent extends NBaseComponent implements OnInit {
     mm: ModelMethods;
-    expenseType=["Perdiem Charges","Airticket/Visa Charges","GuestHouse Charges","Hotel Charges","Onsite Telephone Charges",
-    "Onsite Conveyance Charges","Petrol/Fuel Expenses","Sales Promotion","Staff Welfare Expenses","Travel Food Expenses"]
-     button1='Yes';
-     button2='No';
-     amount=false;
+    expenseType = ["Perdiem Charges", "Airticket/Visa Charges", "GuestHouse Charges", "Hotel Charges", "Onsite Telephone Charges",
+        "Onsite Conveyance Charges", "Petrol/Fuel Expenses", "Sales Promotion", "Staff Welfare Expenses", "Travel Food Expenses"]
+    button1 = 'Yes';
+    button2 = 'No';
+    Amount = false;
+    thumb_nail;
 
-   constructor(private bdms: NDataModelService) {
+
+
+    constructor(private bdms: NDataModelService) {
         super();
         this.mm = new ModelMethods(bdms);
     }
@@ -32,20 +38,86 @@ export class expenseinfoComponent extends NBaseComponent implements OnInit {
 
     }
 
-    accept(option){
-        if(option===this.button1){
-        
-        }
-        else{
-             this.amount=true;
+    // // accept(option) {
+    // //     if (option === this.button1) {
+    //     openCamera(){
+    //         var that = this;
+    //         navigator.camera.getPicture(this.onSuccess, this.onFail, { quality: 100, correctOrientation: true });
+    //     }
+    // }
+    // onFail(message) 
+    // {
+    //     alert('Failed because: ' + message);
+    // }
 
-        }
-    }
+    // onSuccess(imageData)
+    //  {
+    //     textocr.recText(0, 3, imageData, onSuccess, onFail);
+    //     function onSuccess(recognizedText) {
+    //         console.log(recognizedText)
+    //         alert(recognizedText);
+    //     }
+    //     function onFail(message) {
+    //         alert('Failed because: ' + message);
+    //     }
 
 
-    success($event){console.log($event)}
+    // }
 
-    error($event){console.log($event)}
+    //needed code
+
+//  setOptions(srcType) {
+//     var options = {
+//         // Some common settings are 20, 50, and 100
+//         quality: 50,
+//         destinationType: Camera.DestinationType.FILE_URI,
+//         // In this app, dynamically set the picture source, Camera or photo gallery
+//         sourceType: srcType,
+//         encodingType: Camera.EncodingType.JPEG,
+//         mediaType: Camera.MediaType.PICTURE,
+//         allowEdit: true,
+//         correctOrientation: true  //Corrects Android orientation quirks
+//     }
+//     return options;
+// }
+
+ openCamera() {
+     let opts={
+         quality:100,
+         destinationType:1,
+         sourceType:1,
+         encodingType:0,
+         mediaType:0,
+         saveToPhotoAlbum:true,
+         cameraDirection:0,
+         targetWidth:100,
+         targetHeight:100
+
+     }
+     navigator.camera.getPicture(this.cameraSuccess, this.cameraError, opts);
+
+
+ }
+
+cameraSuccess(imageURI){
+// this.thumb_nail.src=imageURI;
+// console.log(this.thumb_nail.src);
+console.log(imageURI);
+
+}
+
+ cameraError(message){
+    console.log('message',message)
+}
+
+ amount()
+ {
+   this.Amount=true;
+ }
+
+
+
+
 
     get(dataModelName, filter?, keys?, sort?, pagenumber?, pagesize?) {
         this.mm.get(dataModelName, filter, keys, sort, pagenumber, pagesize,
@@ -56,6 +128,7 @@ export class expenseinfoComponent extends NBaseComponent implements OnInit {
                 // Handle errors here
             });
     }
+
 
     getById(dataModelName, dataModelId) {
         this.mm.getById(dataModelName, dataModelId,
@@ -99,7 +172,7 @@ export class expenseinfoComponent extends NBaseComponent implements OnInit {
             })
     }
 
-    delete (dataModelName, filter) {
+    delete(dataModelName, filter) {
         this.mm.delete(dataModelName, filter,
             result => {
                 // On Success code here
