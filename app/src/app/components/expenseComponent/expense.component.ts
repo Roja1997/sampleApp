e6dd293233aead1749838d7df3d61606ac7f5a80
rahul9916 <udabal.rahul@gmail.com>
2019-02-20 15:52:46 +0530

    added

--- app/src/app/components/expenseComponent/expense.component.ts
+++ app/src/app/components/expenseComponent/expense.component.ts
@@ -7,7 +7,7 @@ import { NBaseComponent } from '../../../../../app/baseClasses/nBase.component';
 import { otrdetailService } from '../../services/otrDetail/otrdetail.service';
 import { Router } from '@angular/router';
 import { DatePipe } from '@angular/common';
-
+import { MatSnackBar } from '@angular/material';
 /**
  * Service import Example :
  * import { HeroService } from '../services/hero/hero.service';
@@ -28,7 +28,7 @@ export class expenseComponent extends NBaseComponent implements OnInit {
     otrDetail: any = {};
     expensetdetail: any = [];
     totaldays;
-    constructor(private bdms: NDataModelService, private otrdetailService: otrdetailService, private router: Router, private datepipe: DatePipe) {
+    constructor(private bdms: NDataModelService, private otrdetailService: otrdetailService,private snackbar: MatSnackBar, private router: Router, private datepipe: DatePipe) {
         super();
         this.mm = new ModelMethods(bdms);
     }
@@ -52,6 +52,11 @@ export class expenseComponent extends NBaseComponent implements OnInit {
     //pickFromDate fun
     pickFromDate() {
         console.log(this.datepipe.transform(this.fromDate, 'dd/MM/yyyy'));
+        // console.log('checking',this.expensetdetail[0].fromDate);
+        // if(this.expensetdetail[0].fromDate==this.datepipe.transform(this.fromDate, 'dd-MMM-yyyy')){
+        //     this.snackbar.open('already claimed for these OTR add different OTR', 'close', { duration: 3000 });
+        //     this.router.navigate(['/home/expense']);
+        // }
         // this.toDate=new Date(this.fromDate.getTime()+(1000*24*60*60*29));
         this.toDate = new Date(this.fromDate.getFullYear(), this.fromDate.getMonth() + 1, 0);
         console.log('from date', this.fromDate.toDateString());
@@ -60,6 +65,7 @@ export class expenseComponent extends NBaseComponent implements OnInit {
     }
     //submitDate() function
     submitDate() {
+        
         this.otrDetail['fromDate'] = this.datepipe.transform(this.fromDate, 'dd-MMM-yyyy');
         this.otrDetail['toDate'] = this.datepipe.transform(this.toDate, 'dd-MMM-yyyy');
         if (this.expensetdetail == null)
