4a6bc3996b159c333313d2222cb1a07489582480
Roja1997 <rojasanmarga@gmail.com>
2019-03-08 11:18:39 +0530

    reduced variablees

--- app/src/app/components/expenseinfoComponent/expenseinfo.component.ts
+++ app/src/app/components/expenseinfoComponent/expenseinfo.component.ts
@@ -10,7 +10,7 @@ import { DatePipe } from '@angular/common';
 import { Resolve, ActivatedRoute, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
 
 
-/**
+/**  
  * Service import Example :
  * import { HeroService } from '../services/hero/hero.service';
  */
@@ -23,10 +23,10 @@ import { Resolve, ActivatedRoute, ActivatedRouteSnapshot, RouterStateSnapshot, R
 
 export class expenseinfoComponent extends NBaseComponent implements OnInit {
     mm: ModelMethods;
-   
-   
-   
-   
+
+
+
+
     expenseType = ["Perdiem Charges", "Airticket/Visa Charges", "GuestHouse Charges", "Hotel Charges", "Onsite Telephone Charges",
         "Onsite Conveyance Charges", "Petrol/Fuel Expenses", "Sales Promotion", "Staff Welfare Expenses", "Travel Food Expenses"]
     minDate;
@@ -39,24 +39,23 @@ export class expenseinfoComponent extends NBaseComponent implements OnInit {
     bill;
     billName;
     expAmount;
-   
-    comment ;
-   
+    comment;
 
-    otr:any={};
+
+    otr: any = {};
     otrDetail = {};
     expenseDetail = [];
     otrArray = [];
-  
-   
-    
-  
-    
-    
 
 
 
-    constructor(private bdms: NDataModelService,private camService: cameraService, private otrInfo: otrdetailService, private datePipe: DatePipe, private router: Router, private route: ActivatedRoute) {
+
+
+
+
+
+
+    constructor(private bdms: NDataModelService, private camService: cameraService, private otrInfo: otrdetailService, private datePipe: DatePipe, private router: Router, private route: ActivatedRoute) {
         super();
         this.mm = new ModelMethods(bdms);
     }
@@ -69,13 +68,13 @@ export class expenseinfoComponent extends NBaseComponent implements OnInit {
         if (this.otr.expenseList) {
             this.expenseDetail = this.otr.expenseList;
         }
-        var fromDate = this.otr.fromDate;
-        var toDate = this.otr.toDate;
-        this.minDate = this.datePipe.transform(fromDate, "yyyy-MM-dd");
-        this.maxDate = this.datePipe.transform(toDate, "yyyy-MM-dd");
+
+        this.minDate = this.datePipe.transform(this.otr.fromDate, "yyyy-MM-dd");
+        this.maxDate = this.datePipe.transform(this.otr.toDate, "yyyy-MM-dd");
     }
+
+
     //function to disable once the user takes date from date. 
-   
 
     preventuserTyping(event) {
         event.preventDefault();
@@ -85,43 +84,40 @@ export class expenseinfoComponent extends NBaseComponent implements OnInit {
     openCamera() {
         this.img = true;
         this.camService.camera().then((path) => {
-            console.log('image path',path);
+            console.log('image path', path);
             this.imgPath = path;
-           return path;
-            
+            return path;
+
         }).catch((error) => {
             console.log(error);
         });
-  
+
     }
 
-   
 
 
- 
+
+
     submit() {
         this.otrInfo.viewOtr = false;
         this.bill = this.datePipe.transform(this.billDate, "dd-MMM-yyyy");
-        this.otrDetail['Expense_Type'] = this.expType;
-        this.otrDetail['Bill_Name'] = this.billName+" _ "+this.expType;
-        this.otrDetail['Bill_Date'] = this.bill;
-        if (this.otrInfo.country == "South Africa") {
+        this.otrDetail['expType'] = this.expType;
+        this.otrDetail['billName'] = this.billName + " _ " + this.expType;
+        this.otrDetail['billDate'] = this.bill;
 
-            this.expAmount = (this.expAmount * 5.10).toFixed(2);
-        }
-        else if (this.otrInfo.country == "Malaysia") {
+        switch (this.otrInfo.country) {
+            case 'South Africa': this.expAmount = (this.expAmount * 5.10).toFixed(2);
+                break;
 
-            this.expAmount = (this.expAmount * 17.46).toFixed(2);
+            case 'Malaysia': this.expAmount = (this.expAmount * 17.46).toFixed(2);
+                break;
+            case 'Singapore': this.expAmount = (this.expAmount * 52.63).toFixed(2);
+                break;
+            default: this.expAmount = (this.expAmount * 1).toFixed(2);
+                break;
         }
-        else if (this.otrInfo.country == "Singapore") {
-
-            this.expAmount = (this.expAmount * 52.63).toFixed(2);
-        }
-        else {
-            this.expAmount= (this.expAmount * 1).toFixed(2);
-        }
-        this.otrDetail['Expense_Amount'] = this.expAmount;
-        this.otrDetail['Comments'] = this.comment;
+        this.otrDetail['expAmount'] = this.expAmount;
+        this.otrDetail['comment'] = this.comment;
         this.imageurl = this.imgPath;
         this.otrDetail['imageurl'] = this.imageurl;
         this.expenseDetail.push(this.otrDetail);
@@ -129,8 +125,8 @@ export class expenseinfoComponent extends NBaseComponent implements OnInit {
         this.otrArray.push(this.otr);
         this.otrArray.pop();
         localStorage.setItem(this.otrInfo.country, JSON.stringify(this.otrArray));
-        this.otrDetail = {};
-        this.otr = {};
+     //   this.otrDetail = {};
+//this.otr = {};
         this.router.navigate(['/home/expenselist']);
 
     }
